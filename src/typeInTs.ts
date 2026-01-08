88d2c3bec28a2d9ba947e3fc6aef9c8176abba41
Prakash Mani <prakashmanig000@gmail.com>
2026-01-08 17:47:09 +0530

    Duplicate Variable error

---
+++ src/typeInTs.ts
@@ -0,0 +1,15 @@
+let name:string | number = "Prakash Mani"
+name = 0;
+name = "Gautam"
+
+let otp = Math.random() > 0.5 ? 10 : 5;
+
+let username = "Prakash Mani"
+// username = 128   // Type 'number' is not assignable to type 'string'.
+username = "Gautam"  // Currect Way for use 
+
+console.log(otp);
+console.log(username);
+console.log(name);
+
+
